3e330b83a9a80c06088703925cbc7819634cab28
resloved <bfriesen95@gmail.com>
2017-08-04 04:01:54 -0700

    added icon for consistency

--- scroll.sh
+++ scroll.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Specifying the icon(s) in the script
+# This allows us to change its appearance conditionally
+icon=" >"
+
 player_status=$(playerctl status 2> /dev/null)
 if [[ $? -eq 0 ]]; then
     metadata="$(playerctl metadata artist) - $(playerctl metadata title) "
@@ -7,14 +11,13 @@ fi
 
 modifier=4
 length=${#metadata}
-# milliseconds
 cur=$(($(date +%s%3N)/100))
 split=$(($cur/modifier%$length))
 scrolled=${metadata:$split}${metadata:0:$split}
 
 # Foreground color formatting tags are optional
 if [[ $player_status = "Playing" ]]; then
-    echo "%{F#7baa14}$scrolled"       # Orange when playing
+    echo "%{F#7baa14}$icon $scrolled"       # Green when playing
 elif [[ $player_status = "Paused" ]]; then
-    echo "%{F#8a8a8a}$scrolled"       # Greyed out info when paused
+    echo "%{F#8a8a8a}$icon $scrolled"       # Greyed out info when paused
 fi
